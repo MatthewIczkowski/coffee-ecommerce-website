fd42a2105fc7bc55e138ae233a4565611529f493
MatthewIczkowski <iczkom13@gmail.com>
2021-12-31 15:33:17 -0700

    initial commit

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+const Product = ( {product} ) => {
+    const classes = useStyles;
+    let image = "";
+
+    // console.log(product);
+
+    // return <div>test</div>
+
+    if(product.image !== null) {
+        image = product.image.url;
+        console.log(image)
+        console.log(product.name)
+    }
+    console.log(image)
+
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={image} title={product.name} />
+            <CardContent>
+                <div className={classes.cardContnent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price.formatted_with_symbol}
+                    </Typography>
+                </div>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description}} variant="body2" color="textSecondary" />
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default Product
